0b1a188ff0d45d53612ce54b40da84050842ea2f
Marcos <mbjabalquinto@gmail.com>
2026-05-20 20:34:48 +0200

    add funtion to get dimensions from config file in order to be able to renderize de maze

--- render_maze.py
+++ render_maze.py
@@ -20,11 +20,28 @@ def get_maze_coords(maze_file: str) -> list[str]:
     return maze_coords
 
 
+def get_dimensions() -> dict[str, int]:
+    with open("config.txt", "r") as file:
+        dims: dict[str, int] = {}
+        text: list[str] = file.readlines()
+        for line in text:
+            if line.startswith("WIDTH="):
+                dims["WIDTH"] = (int(line.strip().removeprefix("WIDTH=")))
+            elif line.startswith("HEIGHT="):
+                dims["HEIGHT"] = (int(line.strip().removeprefix("HEIGHT=")))
+        if len(dims) < 2:
+            raise Exception("Fatal error: No 'WIDTH' "
+                            "or 'HEIGHT' line in the file")
+        return dims
+
+
 def main() -> None:
     try:
         maze_file: str = (get_file_name())
         maze_coords: list[str] = get_maze_coords(maze_file)
+        dims = get_dimensions()
         print(maze_coords[0][0])  # compruebo que puedo acceder a cada dígito.
+        print(dims)
     except Exception as e:
         print(e)
 
