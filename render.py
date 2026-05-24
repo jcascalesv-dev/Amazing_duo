3f489e45c243f455adb82bb286f7f4f2eb50f866
Marcos <mbjabalquinto@gmail.com>
2026-05-24 13:05:23 +0200

    actualizo render con librería

--- render.py
+++ render.py
@@ -1,67 +1,88 @@
+import sys
 from typing import Any
 from mlx import Mlx
-CELL_SIZE = 32  # constante para el tamano de las texturas en pixels
+
+CELL_SIZE = 32
+WALL_THICKNESS = 4  # El grosor en píxeles de nuestros nuevos archivos .xpm
+MAX_SCREEN_WIDTH = 1920
+MAX_SCREEN_HEIGHT = 1080
+
+
+def transform_directions(
+    entrance: tuple[int, int], directions: str
+) -> set[Any]:
+    x: int
+    y: int
+    x, y = entrance
+    start: list[tuple[int, int]] = []
+    start.append(entrance)
+    for direction in directions:
+        if direction == "N":
+            y -= 1
+        elif direction == "E":
+            x += 1
+        elif direction == "S":
+            y += 1
+        elif direction == "W":
+            x -= 1
+        start.append((x, y))
+    return set(start)
 
 
 def render_maze(
     dims: dict[str, int], maze_coords: list[str], way: dict[str, Any]
 ) -> None:
-    # calculamos el tamano de la ventana en pixels
+    # 1. Calculamos el tamaño final de la ventana
     screen_width: int = dims["WIDTH"] * CELL_SIZE
     screen_height: int = dims["HEIGHT"] * CELL_SIZE
-    # Fabricamos nuestro objeto gráfico a partir del plano
+    # 2. EL ESCUDO ANTI-CRASHES
+    if screen_width > MAX_SCREEN_WIDTH or screen_height > MAX_SCREEN_HEIGHT:
+        print("Error Crítico: El laberinto es demasiado "
+              "grande para la interfaz gráfica "
+              f"({screen_width}x{screen_height} px). El límite seguro es "
+              f"{MAX_SCREEN_WIDTH}x{MAX_SCREEN_HEIGHT} px. "
+              "Por favor, use el modo de renderizado ASCII.", file=sys.stderr)
+        sys.exit(1)
+    # 3. Inicializamos la librería gráfica
     mlx_visual = Mlx()
-    # Guardamos el identificador del motor gráfico
     mlx_ptr = mlx_visual.mlx_init()
-    # Estamos usando un traductor ya que la librería está hecha en c
-    # pero no tiene tipado así que lo silenciamos.
     window_ptr: int = mlx_visual.mlx_new_window(  # type: ignore
-        mlx_ptr, screen_width, screen_height, "A-maze-ing!"
+        mlx_ptr, screen_width, screen_height, "A-maze-ing MLX!"
     )
-    # Cargamos la textura para el muro. Descomponemos en 3 variables
-    # pero solo necesitamos la imagen puesto que las medidas ya las tenemos.
-    img_wall_north, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
-        mlx_ptr, "textures/wall_north.xpm"
-        )
-    img_wall_east, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
-        mlx_ptr, "textures/wall_east.xpm"
-        )
-    img_wall_south, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
-        mlx_ptr, "textures/wall_south.xpm"
-        )
-    img_wall_west, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
-        mlx_ptr, "textures/wall_west.xpm"
-        )
+    # 4. CARGAMOS LAS DOS TEXTURAS MAESTRAS
+    img_wall_h, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
+        mlx_ptr, "textures/wall_h.xpm"
+    )
+    img_wall_v, _, _ = mlx_visual.mlx_xpm_file_to_image(  # type: ignore
+        mlx_ptr, "textures/wall_v.xpm"
+    )
+    # 5. EL BUCLE DE RENDERIZADO DE MUROS
     for y, row in enumerate(maze_coords):
         for x, digit in enumerate(row):
             decimal: int = int(digit, 16)
             pixel_x = x * CELL_SIZE
             pixel_y = y * CELL_SIZE
-            # 1. LA LÓGICA ASCII: Solo pintamos Norte y Oeste.
-            # Las vecinas harán el resto.
-            if decimal & 1:  # NORTE
+            # --- A. LÓGICA DE MUROS INTERNOS (Norte y Oeste) ---
+            if decimal & 1:  # NORTE(bloque horizontal en el techo de la celda)
                 mlx_visual.mlx_put_image_to_window(  # type: ignore
-                    mlx_ptr, window_ptr, img_wall_north, pixel_x, pixel_y
+                    mlx_ptr, window_ptr, img_wall_h, pixel_x, pixel_y
                 )
-            if decimal & 8:  # OESTE
+            if decimal & 8:  # OESTE (bloque vertical en la pared izquierda)
                 mlx_visual.mlx_put_image_to_window(  # type: ignore
-                    mlx_ptr, window_ptr, img_wall_west, pixel_x, pixel_y
+                    mlx_ptr, window_ptr, img_wall_v, pixel_x, pixel_y
                 )
-            # 2. CERRAR LOS BORDES (Igual que el ASCII)
-            # Si es la ÚLTIMA celda de la fila, tenemos que pintar su muro 
-            # Este para cerrar el mapa
+            # --- B. CIERRE PERIMETRAL EXTERIOR ---
+            # Borde Este (última colum):Empujamos el muro vertical a la derecha
             if x == len(row) - 1 and (decimal & 2):
                 mlx_visual.mlx_put_image_to_window(  # type: ignore
-                    mlx_ptr, window_ptr, img_wall_east, pixel_x + CELL_SIZE - 2, pixel_y
+                    mlx_ptr, window_ptr, img_wall_v, 
+                    pixel_x + CELL_SIZE - WALL_THICKNESS, pixel_y
                 )
-            # Si es la ÚLTIMA fila del laberinto, tenemos que pintar su muro 
-            # Sur para cerrar el mapa
+            # Borde Sur (última fila): Empujamos el muro horizontal hacia abajo
             if y == len(maze_coords) - 1 and (decimal & 4):
                 mlx_visual.mlx_put_image_to_window(  # type: ignore
-                    mlx_ptr, window_ptr, img_wall_south,
-                    pixel_x, pixel_y + CELL_SIZE - 2
+                    mlx_ptr, window_ptr, img_wall_h, 
+                    pixel_x, pixel_y + CELL_SIZE - WALL_THICKNESS
                 )
-    # Llamamos al método de la librería que es un bucle infinito
-    # para evitar que la ventana se cierre tras renderizar el
-    # laberinto y quede a la espera.
+    # 6. Mantener la ventana abierta
     mlx_visual.mlx_loop(mlx_ptr)  # type: ignore
