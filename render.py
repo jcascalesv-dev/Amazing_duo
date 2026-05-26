f581311631711d187426175708fcec9f7a2a1ae1
Marcos <mbjabalquinto@gmail.com>
2026-05-26 15:05:46 +0200

    exit

--- render.py
+++ render.py
@@ -128,7 +128,7 @@ def render_maze(
                     )
                 elif coord == way["exit"]:
                     mlx_visual.mlx_put_image_to_window(  # type: ignore
-                        mlx_ptr, window_ptr, img_exit, pixel_x, pixel_y
+                        mlx_ptr, window_ptr, img_exit, pixel_x + 1, pixel_y
                     )
                 elif decimal == 15:
                     # El bloque masivo del 42 rellena
